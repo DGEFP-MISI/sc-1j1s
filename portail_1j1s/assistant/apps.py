09504954d8da293311f46c8ff27c9e82e4eae7f7
Charles Dechamps <charles.dechamps@emploi.gouv.fr>
2026-09-19 17:06:57 +0200

    Create apps.py
    Création de l'application Django assistant IA

---
+++ portail_1j1s/assistant/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class AssistantConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "portail_1j1s.assistant"
+    verbose_name = "Assistant IA 1jeune1solution"
